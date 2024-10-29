09f1962c23080c7923d14ec644f7324a8e985983
AndricaNuta <andrica.nuta@gmail.com>
2024-10-29 12:31:50 +0200

    Update performance

--- hooks/useFetchParkings.ts
+++ hooks/useFetchParkings.ts
@@ -2,23 +2,23 @@ import { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
 import { ParkingData } from '../types/parkingData';
 
+const PARKING_URL = 'https://data.stad.gent/api/explore/v2.1/catalog/datasets/bezetting-parkeergarages-real-time/records?limit=20';
+
 export const useFetchParkings = () => {
   const [parkings, setParkings] = useState<ParkingData[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [refreshing, setRefreshing] = useState(false);
-  const url = 'https://data.stad.gent/api/explore/v2.1/catalog/datasets/bezetting-parkeergarages-real-time/records?limit=20';
 
-  const fetchParkingData = useCallback(async(isInitialLoad = false) => {
-    if (isInitialLoad) setLoading(true);
+  const fetchParkingData = useCallback(async (isRefreshing = false) => {
+    isRefreshing ? setRefreshing(true) : setLoading(true);
 
     try {
-      const response = await axios.get(url);
-
+      const response = await axios.get<{ results: ParkingData[] }>(PARKING_URL);
       const filteredParkings = response.data.results
-        .filter((parking: ParkingData) => parking.occupation <= 50)
-        .sort((a: ParkingData, b: ParkingData) => a.occupation - b.occupation)
-        .map((parking: ParkingData) => ({
+        .filter((parking) => parking.occupation <= 50)
+        .sort((a, b) => a.occupation - b.occupation)
+        .map((parking) => ({
           id: parking.id,
           name: parking.name,
           totalcapacity: parking.totalcapacity,
@@ -28,28 +28,22 @@ export const useFetchParkings = () => {
           location: parking.location,
         }));
 
-      setParkings((prevParkings) =>
-        JSON.stringify(prevParkings) === JSON.stringify(filteredParkings) ? prevParkings : filteredParkings
-      );
-      setError(null); // Clear error on success
+      setParkings((prev) => JSON.stringify(prev) === JSON.stringify(filteredParkings) ? prev : filteredParkings);
     } catch (err) {
       console.error("Error fetching parking data:", err);
       setError("Failed to load data.");
     } finally {
-      setLoading(false);
-      setRefreshing(false);
+      isRefreshing ? setRefreshing(false) : setLoading(false);
     }
-  }, [url, refreshing]); 
+  }, []);
 
   const onRefresh = useCallback(() => {
-    setRefreshing(true);
-    fetchParkingData();
-  }, [fetchParkingData]); 
-
+    fetchParkingData(true);
+  }, [fetchParkingData]);
 
   useEffect(() => {
     fetchParkingData();
-  }, []); 
+  }, [fetchParkingData]);
 
   return { parkings, loading, error, refreshing, onRefresh };
-};  
+};
